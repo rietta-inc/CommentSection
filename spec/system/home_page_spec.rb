6362a4033f8d058b920066ebb82fcb6875bee1ba
Frank Rietta <frank@rietta.com>
2019-11-06 22:36:47 -0500

    Adds test for e-mail field validation message

--- spec/system/home_page_spec.rb
+++ spec/system/home_page_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe 'Home Page Marketing Content', type: :system do
     visit '/'
     expect(page).to have_text 'Welcome to the Comments Section'
   end
+
+  it 'requires e-mail in e-mail field' do
+    visit root_path
+    fill_in 'E-mail', with: 'bob'
+    click_on 'Subscribe'
+    validation_message = page.find('#_signup_email').native.attribute("validationMessage")
+    expect(validation_message).to have_text "Please include an '@' in the email address."
+  end
 end
